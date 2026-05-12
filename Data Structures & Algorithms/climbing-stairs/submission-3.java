7035b9f9b827f741dbff1623d3f67dc4db3bfd47
Niranjan C B <119934859+niru128@users.noreply.github.com>
2026-05-12 21:40:06 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int[] dp = new int[n + 1];
+       return helper(n,dp);
+    }
+
+    int helper(int n, int[] dp){
+
+
+        if(n <= 2) return n;
+
+
+        if(dp[n] != 0){
+            return dp[n];
+        }
+
+        dp[n] = helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
+
+    }
+}
